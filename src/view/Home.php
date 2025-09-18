eb24c1523c74ead78af8679c704c17444e502f98
Otavio1661 <otavio.silva1661@gmail.com>
2025-09-18 11:24:39 -0300

    ajustes

--- src/view/Home.php
+++ src/view/Home.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo; ?></title>
-
+    <link rel="icon" href="/img/aaaaa2.png" type="image/png">
     <style>
         body {
             font-family: Arial, sans-serif;
