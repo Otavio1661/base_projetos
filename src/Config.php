3f4d4a54d7b65ff7ecda8e6455e532a8c9fe32fe
Otavio1661 <otavio.silva1661@gmail.com>
2025-10-23 17:08:22 -0300

    ajustes_config

--- src/Config.php
+++ src/Config.php
@@ -6,10 +6,13 @@ require_once __DIR__ . '/Env.php';
 
 class Config
 {
+
+// ----------------- Definições do banco de dados -----------------
     const DB_HOST_LG  = DB_HOST_LG;
     const DB_HOST     = DB_HOST;
     const DB_PORT     = DB_PORT;
     const DB_DATABASE = DB_DATABASE;
     const DB_USERNAME = DB_USERNAME;
     const DB_PASSWORD = DB_PASSWORD;
+// ----------------------------------------------------------------
 }
